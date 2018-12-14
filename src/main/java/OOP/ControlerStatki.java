82f16bc3839553e8e938bc3c67e54bdfcbba0297
Kamil Ochnik <ilmak@o2.pl>
2018-12-14 17:39:47 +0100

    jak zrobic dwie slisty statkow?

--- src/main/java/OOP/ControlerStatki.java
+++ src/main/java/OOP/ControlerStatki.java
@@ -5,12 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ControlerStatki {
 
     @RequestMapping("/")
     public String wyswierlanie (){
         return "walka";
+    }
+
+    @RequestMapping("/all")
+    public String listatStatkow (
+            Model model,
+            @RequestParam(value = "nacja", required = false) Nacja nacja
+    ){
+        List<Statek> statkiGracza1 = new ArrayList<>();
+        List<Statek> statkiGracza2 = new ArrayList<>();
+
+        if (nacja.equals(Nacja("Gallente"))) {
+            statkiGracza1.add(statek);
+        } else statkiGracza2.add(statek);
+
+
+        model.addAttribute("nacja", statkiGracza1);
+        return "nacja";
     }
 
 
--- src/main/java/OOP/DemoTestStatki.java
+++ src/main/java/OOP/DemoTestStatki.java
@@ -1,6 +1,9 @@
 package OOP;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class DemoTestStatki {
@@ -10,6 +13,12 @@ public class DemoTestStatki {
         System.setProperty("JDBC_DATABASE_URL", "jdbc");
         TestStatki.main(args);
     }
+
+
+
+
+
+
 }
 
 
