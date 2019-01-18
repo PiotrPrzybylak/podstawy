824594e5f37a0164316876e85d6b9bf1d8be88e1
Kamil Ochnik <ilmak@o2.pl>
2019-01-18 13:19:12 +0100

    statkiGracza1 - 3 usunałem z kodu ( w komunikacji z htmlem jeszcze istnieje)

--- src/main/java/OOP/ControlerStatki.java
+++ src/main/java/OOP/ControlerStatki.java
@@ -11,34 +11,30 @@ import java.util.List;
 @Controller
 public class ControlerStatki {
 
-
-    private List <Statek> statkiGracza1 = new ArrayList <>();
-    private List <Statek> statkiGracza2 = new ArrayList <>();
-    private List <Statek> statkiGracza3 = new ArrayList <>();
     private List <StatekSklep> statkiDoKulepinia = new ArrayList <>();
     private List <Statek> statkiZlomowisko = new ArrayList<>();
     private int kosztStatku = 0;
 
-    Gracz gracz1= new Gracz(statkiGracza1,1000000);
-    Gracz gracz2= new Gracz(statkiGracza2,1000000 );
-    Gracz gracz3= new Gracz(statkiGracza3,1000000);
+    Gracz gracz1= new Gracz(new ArrayList <>(),1000000);
+    Gracz gracz2= new Gracz(new ArrayList <>(),1000000 );
+    Gracz gracz3= new Gracz(new ArrayList <>(),1000000);
 
 
     public ControlerStatki (){
-        statkiGracza1.add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
-        statkiGracza1.add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
-        statkiGracza1.add(new Fregata(new Nacja("Gallente"), "zosia", "Tristan", 100, 5600));
-        statkiGracza1.add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
+        gracz1.getStatki().add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
+        gracz1.getStatki().add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
+        gracz1.getStatki().add(new Fregata(new Nacja("Gallente"), "zosia", "Tristan", 100, 5600));
+        gracz1.getStatki().add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
 
-        statkiGracza2.add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
-        statkiGracza2.add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
-        statkiGracza2.add(new Fregata(new Nacja("Gallente"), "zosia2", "Tristan", 100, 5600));
-        statkiGracza2.add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
+        gracz2.getStatki().add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
+        gracz2.getStatki().add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
+        gracz2.getStatki().add(new Fregata(new Nacja("Gallente"), "zosia2", "Tristan", 100, 5600));
+        gracz2.getStatki().add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
 
-        statkiGracza3.add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
-        statkiGracza3.add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
-        statkiGracza3.add(new Fregata(new Nacja("Gallente"), "zosia", "Tristan", 100, 5600));
-        statkiGracza3.add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
+        gracz3.getStatki().add( new Krazownik(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565));
+        gracz3.getStatki().add(new Krazownik(new Nacja("Amarr"),"Krążownik","Maller", 294,35545));
+        gracz3.getStatki().add(new Fregata(new Nacja("Gallente"), "zosia", "Tristan", 100, 5600));
+        gracz3.getStatki().add(new Fregata(new Nacja("Gallente"), "Fregata", "Atron", 4000,4000));
 
         statkiDoKulepinia.add( new StatekSklep(new Nacja("Gallente"), "Krążownik","Vexor", 300,53565, 30000));
         statkiDoKulepinia.add(new StatekSklep(new Nacja("Amarr"),"Krążownik","Maller", 294,35545, 33000));
@@ -58,13 +54,13 @@ public class ControlerStatki {
         int kasaGracza = 0;
 
         if (ktoSprzedaje.equals("Gracz 1")) {
-            statkiNaSprzedaz = statkiGracza1;
+            statkiNaSprzedaz = gracz1.getStatki();
             kasaGracza = gracz1.getKasa();
         } else if (ktoSprzedaje.equals("Gracz 2")) {
-            statkiNaSprzedaz = statkiGracza2;
+            statkiNaSprzedaz = gracz2.getStatki();
             kasaGracza = gracz2.getKasa();
         } else if (ktoSprzedaje.equals("Gracz 3")) {
-            statkiNaSprzedaz = statkiGracza3;
+            statkiNaSprzedaz = gracz3.getStatki();
             kasaGracza = gracz3.getKasa();
         }
 
@@ -88,13 +84,13 @@ public class ControlerStatki {
             }
 
         if (ktoSprzedaje.equals("Gracz 1")) {
-            statkiGracza1 = statkiNaSprzedaz;
+            gracz1.setStatki(statkiNaSprzedaz);
             gracz1.setKasa(kasaGracza);
         } else if (ktoSprzedaje.equals("Gracz 2")) {
-            statkiGracza2 = statkiNaSprzedaz;
+            gracz2.setStatki(statkiNaSprzedaz);
             gracz2.setKasa(kasaGracza);
         } else if (ktoSprzedaje.equals("Gracz 3")) {
-            statkiGracza3 = statkiNaSprzedaz;
+            gracz3.setStatki(statkiNaSprzedaz);
             gracz3.setKasa(kasaGracza);
         }
             return "redirect:/sprzedaz?ktoSprzedaje="+ktoSprzedaje;
@@ -129,13 +125,13 @@ public class ControlerStatki {
 
                 if (ktoKupuje.equals("Gracz 1")) {
                     gracz1.setKasa(kasaGracza);
-                    statkiGracza1.add(tylkoStatek);
+                    gracz1.getStatki().add(tylkoStatek);
                 } else if (ktoKupuje.equals("Gracz 2")) {
                     gracz2.setKasa(kasaGracza);
-                    statkiGracza2.add(tylkoStatek);
+                    gracz2.getStatki().add(tylkoStatek);
                 } else if (ktoKupuje.equals("Gracz 3")) {
                     gracz3.setKasa(kasaGracza);
-                    statkiGracza3.add(tylkoStatek);
+                    gracz3.getStatki().add(tylkoStatek);
                 }
             }
         }
@@ -167,13 +163,13 @@ public class ControlerStatki {
         int kasaGracza = 0;
 
         if (ktoSprzedaje.equals("Gracz 1")) {
-            statkiNaSprzedaz = statkiGracza1;
+            statkiNaSprzedaz = gracz1.getStatki();
             kasaGracza = gracz1.getKasa();
         } else if (ktoSprzedaje.equals("Gracz 2")) {
-            statkiNaSprzedaz = statkiGracza2;
+            statkiNaSprzedaz = gracz2.getStatki();
             kasaGracza = gracz2.getKasa();
         } else if (ktoSprzedaje.equals("Gracz 3")) {
-            statkiNaSprzedaz = statkiGracza3;
+            statkiNaSprzedaz = gracz3.getStatki();
             kasaGracza = gracz3.getKasa();
         }
 
@@ -228,57 +224,57 @@ public class ControlerStatki {
         // teraz IFy dla walka dla wszystkich opcji
 
         if (ktoWalczy.equals("Gracz 1+2")) {
-            // foreach po liscie statkiGracza1
-            for (int i = 0; i < statkiGracza1.size(); i++) {
-                Statek statek = statkiGracza1.get(i);
+            // foreach po liscie statki Gracza1
+            for (int i = 0; i < gracz1.getStatki().size(); i++) {
+                Statek statek = gracz1.getStatki().get(i);
 
                 if (statekGracza1.equals(statek.getNazwa())) {
-                    pierwszy = statkiGracza1.get(i);
+                    pierwszy = gracz1.getStatki().get(i);
                 }
             }
 
-            // foreach po liscie statkiGracza2
-            for (int i = 0; i < statkiGracza2.size(); i++) {
-                Statek statek = statkiGracza2.get(i);
+            // foreach po liscie statki Gracza2
+            for (int i = 0; i < gracz2.getStatki().size(); i++) {
+                Statek statek = gracz2.getStatki().get(i);
 
                 if (statekGracza2.equals(statek.getNazwa())) {
-                    drugi = statkiGracza2.get(i);
+                    drugi = gracz2.getStatki().get(i);
                 }
             }
         } else if (ktoWalczy.equals("Gracz 2+3")) {
-            // foreach po liscie statkiGracza2
-            for (int i = 0; i < statkiGracza2.size(); i++) {
-                Statek statek = statkiGracza2.get(i);
+            // foreach po liscie statki Gracza2
+            for (int i = 0; i < gracz2.getStatki().size(); i++) {
+                Statek statek = gracz2.getStatki().get(i);
 
                 if (statekGracza2.equals(statek.getNazwa())) {
-                    pierwszy = statkiGracza2.get(i);
+                    pierwszy = gracz2.getStatki().get(i);
                 }
             }
 
-            // foreach po liscie statkiGracza3
-            for (int i = 0; i < statkiGracza3.size(); i++) {
-                Statek statek = statkiGracza3.get(i);
+            // foreach po liscie statki Gracza3
+            for (int i = 0; i < gracz3.getStatki().size(); i++) {
+                Statek statek = gracz3.getStatki().get(i);
 
                 if (statekGracza3.equals(statek.getNazwa())) {
-                    drugi = statkiGracza3.get(i);
+                    drugi = gracz3.getStatki().get(i);
                 }
             }
         } else if (ktoWalczy.equals("Gracz 3+1")) {
-            // foreach po liscie statkiGracza3
-            for (int i = 0; i < statkiGracza3.size(); i++) {
-                Statek statek = statkiGracza3.get(i);
+            // foreach po liscie statki Gracza3
+            for (int i = 0; i < gracz3.getStatki().size(); i++) {
+                Statek statek = gracz3.getStatki().get(i);
 
                 if (statekGracza3.equals(statek.getNazwa())) {
-                    pierwszy = statkiGracza3.get(i);
+                    pierwszy = gracz3.getStatki().get(i);
                 }
             }
 
-            // foreach po liscie statkiGracza1
-            for (int i = 0; i < statkiGracza1.size(); i++) {
-                Statek statek = statkiGracza1.get(i);
+            // foreach po liscie statki Gracza1
+            for (int i = 0; i < gracz1.getStatki().size(); i++) {
+                Statek statek = gracz1.getStatki().get(i);
 
                 if (statekGracza1.equals(statek.getNazwa())) {
-                    drugi = statkiGracza1.get(i);
+                    drugi = gracz1.getStatki().get(i);
                 }
             }
         }
@@ -305,9 +301,9 @@ public class ControlerStatki {
     ){
 
 // chce zrobic commita bo na lapku nie dziala i sprawdzic tak cala sprawe
-        model.addAttribute("statkiGracza1", this.statkiGracza1);
-        model.addAttribute("statkiGracza2", statkiGracza2);
-        model.addAttribute("statkiGracza3", statkiGracza3);
+        model.addAttribute("statkiGracza1", gracz1.getStatki());
+        model.addAttribute("statkiGracza2", gracz2.getStatki());
+        model.addAttribute("statkiGracza3", gracz3.getStatki());
         return "walka";
     }
 
