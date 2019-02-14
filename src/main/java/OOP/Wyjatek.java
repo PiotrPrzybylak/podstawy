5c28326bc51d81778586ce2a97f9958786d3fc6f
Kamil Ochnik <ilmak@o2.pl>
2019-02-14 12:08:45 +0100

    staram sie naprawic wyswietlanie bledu w przegladarce

--- src/main/java/OOP/Wyjatek.java
+++ src/main/java/OOP/Wyjatek.java
@@ -4,11 +4,16 @@ public class Wyjatek extends Exception {
     private String informacja;
 
 
+    public String getMessage() {
+        return "Nie ma takiego numeru gracza "+informacja;
+    }
+
     public Wyjatek (String s){
         informacja = s;
     }
 
-    public String toString  (){
+    public String toString (){
         return "Nie ma takiego numeru gracza "+informacja;
     }
+
 }
