b7be612b18b26802cb230af70fc57fe5b74ee9ca
Kamil Ochnik <ilmak@o2.pl>
2019-02-14 13:04:17 +0100

    dodalem inny rodzaj wyjatku

--- src/main/java/OOP/Wyjatek.java
+++ src/main/java/OOP/Wyjatek.java
@@ -1,19 +1,18 @@
 package OOP;
 
+//Wyjatek typu un checked!!
+//public class Wyjatek extends RuntimeException {
+//Wyjatek typu checked!!
 public class Wyjatek extends Exception {
     private String informacja;
-
 
     public String getMessage() {
         return "Nie ma takiego numeru gracza "+informacja;
     }
-
     public Wyjatek (String s){
         informacja = s;
     }
-
     public String toString (){
         return "Nie ma takiego numeru gracza "+informacja;
     }
-
 }
